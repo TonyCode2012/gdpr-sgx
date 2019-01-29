9ccdd9b2381372022660ce92835804e7074c56f6
mengzhu <xiemengzhu0526@gmail.com>
2019-01-29 18:05:10 +0800

    transfrom userID from string to Uint8Array

--- broswer-application/src/utils/messages/smsMsg.js
+++ broswer-application/src/utils/messages/smsMsg.js
@@ -1,11 +1,14 @@
 import {
   SMS_SEND
 } from "../../metadata/messageTypes";
+import {
+  hexStringToArray
+} from "../hexHelpers";
 
 const getSmsMsg = ({ userID, content }) => {
   return {
     type: SMS_SEND,
-    userID,
+    userID: Uint8Array.from(hexStringToArray(userID, 2)),
     sms: content,
     size: 0
   };
